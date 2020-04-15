ee8734e332c45bd80240efc8f2b01256d70a821a
Eduardo Viana <eduardocosta_viana@live.com>
2020-04-15 13:24:30 -0300

    Adicionado configurações para o raspberry

---
+++ .docker/raspberry/build/precache-manifest.98f4826070da18f25d42a202b96ebdeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea567d23f39840e9f851b5fcad9239ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d49d96133b9d642f324c",
+    "url": "/static/css/2.fdb06ec3.chunk.css"
+  },
+  {
+    "revision": "d49d96133b9d642f324c",
+    "url": "/static/js/2.afa92e20.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.afa92e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6b3929b3bb358c5715",
+    "url": "/static/js/main.6783a083.chunk.js"
+  },
+  {
+    "revision": "e6c3996783f065ad772b",
+    "url": "/static/js/runtime-main.23c225d8.js"
+  },
+  {
+    "revision": "cfb728893c59e53d78343a0b7a708f5e",
+    "url": "/static/media/bg-mobilis.cfb72889.jpg"
+  },
+  {
+    "revision": "73d989b1f769ab9c51adfd06f8f373a8",
+    "url": "/static/media/logo-satc-horizontal.73d989b1.png"
+  }
+]);
